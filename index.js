fd65fc2d18a2e02292f7c04f9b60df779984ddf5
Cynthia LeGolvan <cynthia.nguyen21@gmail.com>
2019-02-14 04:01:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 function dwarfRollCall(dwarves) {
+  var dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+  var numbersNames = []
+  for (var i = 0; i < dwarves.length; i++) {
+    numbersNames.push(`${i + 1}. ${dwarves[i]}`)
+  }
+ return `${numbersNames.join('')}` 
 }
 
 function summonCaptainPlanet(planeteerCalls){
